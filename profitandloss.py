178005a28d659b215c13f1d776ebecce3e6765d7
sheryllchuaa <158441038+sheryllchuaa@users.noreply.github.com>
2024-02-01 14:38:15 +0800

    Update profitandloss.py

--- profitandloss.py
+++ profitandloss.py
@@ -50,4 +50,40 @@ def get_net_profit_difference(file_path):
         res[day] = diff
 
     return res
+def get_output(profit_diff):
+    '''
+    Accepts a dict as argument.
+    Returns output according to trend.
+    '''
+    trend = get_trend(profit_diff)
+    if trend == "increase":
+        return get_increasing_output(profit_diff)
+    elif trend == "decreasse":
+        return get_decreasing_output(profit_diff)
+    else:
+        return get_fluctuating_output(profit_diff)
 
+
+def get_trend(profit_diff):
+    '''
+    Accepts a dict as argument.
+    Returns trend as string output.
+    '''
+    increasing = False
+    decreasing = False
+    for diff in profit_diff.values():
+        # If both increasing and decrease has occured in middle of loop, break loop.
+        if increasing and decreasing:
+            break
+
+        if diff > 0:
+            increasing = True
+        elif diff < 0:
+            decreasing = True
+    
+    if increasing and decreasing:
+        return "fluctuate"
+    elif increasing:
+        return "increase"
+    else:
+        return "decrease"
