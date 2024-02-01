d57424ac7b7bad1da85e71ee765de948251aefee
HazimahHamsan <158028105+HazimahHamsan@users.noreply.github.com>
2024-02-01 14:58:40 +0800

    Update main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import cash_on_hand
+import overheads
+import ProfitandLoss
+
+def main():
+    overheads.overhead_function()
+    cash_on_hand.coh_function()
+    ProfitandLoss.profit_loss_function()
+
+main()
