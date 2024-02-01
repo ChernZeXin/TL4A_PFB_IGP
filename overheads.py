c48ead95cc6027c1930e7ccdedf763a4ef066a3e
ChernZeXin <158028010+ChernZeXin@users.noreply.github.com>
2024-02-01 15:00:20 +0800

    Update overheads.py

--- overheads.py
+++ overheads.py
@@ -12,3 +12,20 @@ def read_and_process_file():
             records.append([row[0], float(row[1])])
             
     return records
+
+def identify_highest_overhead_and_generate_output(overheads):
+    percentage_overheads = []
+    for row in overheads:
+        percentage_overheads.append(row[1])
+
+    output = f'[HIGHEST OVERHEAD] {overheads[percentage_overheads.index(max(percentage_overheads))][0]}: {overheads[percentage_overheads.index(max(percentage_overheads))][1]}%\n'
+    return output
+
+def overhead_function():
+    # Read and process the CSV file
+    records = read_and_process_file()
+    # 
+    output = identify_highest_overhead_and_generate_output(records)
+    # Write the output to the Summary_report.txt file
+    with open('Summary_report.txt', 'w') as f:
+        f.write(output)
